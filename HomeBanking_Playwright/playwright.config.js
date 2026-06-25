21f5b7e62ee73d914bb2c64caae0059b2e391bf1
Esteban Joel <gaunaesteban983@gmail.com>
2026-06-25 02:54:52 -0300

    .

--- HomeBanking_Playwright/playwright.config.js
+++ HomeBanking_Playwright/playwright.config.js
@@ -5,42 +5,30 @@ export default defineConfig({
 
   testDir: './tests',
 
-  // 🔥 IMPORTANTE: evita conflictos entre tests en CI
   fullyParallel: false,
 
   forbidOnly: !!process.env.CI,
 
   retries: process.env.CI ? 2 : 0,
 
-  // 🔥 clave para estabilidad en CI
   workers: process.env.CI ? 1 : undefined,
 
   reporter: 'html',
 
   use: {
 
-    // CI = headless / Local = visible
     headless: !!process.env.CI,
 
     viewport: null,
 
-    trace: 'on',
+    trace: 'retain-on-failure',
 
-    screenshot: 'on',
+    screenshot: 'only-on-failure',
 
-    video: {
-      mode: 'on',
-      size: {
-        width: 1920,
-        height: 1080,
-      }
-    },
+    video: 'retain-on-failure',
 
     launchOptions: {
-
-      // CI rápido / local con delay para debug
       slowMo: process.env.CI ? 0 : 500,
-
       args: ['--start-maximized'],
     },
   },
